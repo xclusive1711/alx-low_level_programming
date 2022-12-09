df45fbc602c86a76b2167cb60d84ce6a963e8d6b
Olayiwola Solomon <xclusive1711@gmail.com>
2022-12-09 07:27:49 -0800

    new commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints in lowercase
+ * followed by newline except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
