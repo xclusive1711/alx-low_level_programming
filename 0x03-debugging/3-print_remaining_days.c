7663428dd8f2d297b0a81654659e6deb76213581
Olayiwola Solomon <xclusive1711@gmail.com>
2022-12-14 07:47:50 -0800

    new commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,9 +11,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0 || year % 4 == 0 && year % 100 == 0))
+	if ((year % 100 == 0 || year % 400 == 0 && year % 4 == 0))
 	{
-		if (month >= 3 && day >= 60)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
@@ -30,8 +29,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
