2e8f90f7ea957ad3bd2b72401af86c152600a87f
Olayiwola Solomon <xclusive1711@gmail.com>
2022-12-15 04:58:05 -0800

    new commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * void print_numbers - prints number from 0 to 9
  * followed by a new line
- * Return: no returns
+ * Return: void(no returns)
  */
 
 void print_numbers(void)
@@ -11,6 +11,8 @@ void print_numbers(void)
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
+	{
 		_putchar(c);
+	}
 	_putchar('\n');
 }
