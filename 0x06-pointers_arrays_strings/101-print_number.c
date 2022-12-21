5069b24da66b1ae52437ac8986ee93d90e2a2fbb
Olayiwola Solomon <xclusive1711@gmail.com>
2022-12-21 13:25:04 -0800

    new commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
+
 /**
  * print_number - prints an integer
- * @n: The integer to be printed.
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num = nn;
 
 	if (n < 0)
 	{
 		_putchar('_');
 		num = -num;
 	}
+
 	if ((num / 10) > 0)
 		print_number(num / 10);
 
-	_putchar((num % 10) + '0');
+	_putchar((num % 10 + '0');
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,7 +15,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  */
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-	int num,tens=0;
+	int num, tens = 0;
 
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
@@ -25,24 +25,24 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		tens = num / 10;
 	}
 
-	 for (; *n1; n1--, r_index--)
+	for (; *n1; n1--, r_index--)
 	{
 		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
-	 for (; *n2; n2--, r_index--)
+	for (; *n2; n2--, r_index--)
 	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
-	 if (tens && r_index >= 0)
+	if (tens && r_index >= 0)
 	{
 		*(r + r_index) = (tens % 10) + '0';
 		return (r + r_index);
 	}
-	 else if (tens && r_index < 0)
-		 return (0);
+	else if (tens && r_index < 0)
+		return (0);
 	return (r + r_index + 1);
 }
