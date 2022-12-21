a66a33e2c6794aab8eb59e9bfbc09566900f9429
Olayiwola Solomon <xclusive1711@gmail.com>
2022-12-21 13:03:53 -0800

    new commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,17 +10,13 @@ void print_number(int n)
 {
 	unsigned int n1;
 
-	n1 = n;
-
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+		_putchar('_');
+		num = -num;
 	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	_putchar((num % 10) + '0');
 }
