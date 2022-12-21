b06b7886c6e51f4a80a1326a1297e26550246369
Olayiwola Solomon <xclusive1711@gmail.com>
2022-12-21 13:10:08 -0800

    new commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
-  * print_number - print numbers chars
-  * @n: integer params
-  * Return: 0
- **/
-
+ * print_number - prints an integer
+ * @n: The integer to be printed.
+ */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
